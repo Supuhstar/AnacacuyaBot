f53f2da11bd5e7dd2dd3232f26ff6596177f38b1
The Northstar✨ System <North@Supuhstar.com>
2026-05-25 23:26:34 -0600

    Removing the token limit for now

--- Sources/AnacacuyaBot/Bot/Persona.swift
+++ Sources/AnacacuyaBot/Bot/Persona.swift
@@ -17,7 +17,7 @@ extension Persona {
         pronouns: "they/them",
         fursona: "a gryphon",
         
-        modelSettings: .init(num_predict: 500),
+//        modelSettings: .init(num_predict: 500),
         
         directResponseSystemPrompt: """
             Keep your reply to 1~3 sentences at MOST.
